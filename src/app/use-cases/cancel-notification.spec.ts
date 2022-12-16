bc2794d38c875f8c30ae30f53ff60b92a9d92d6e
Renan Costa <renancostam@gmail.com>
2022-12-16 20:19:13 -0300

    Test adjustments

--- src/app/use-cases/cancel-notification.spec.ts
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -1,13 +1,14 @@
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
 import { SendNotification } from './send-notification';
+
+const notificationsRepository = new InMemoryNotificationsRepository();
+const sendNotification = new SendNotification(notificationsRepository);
+const cancelNotification = new CancelNotification(notificationsRepository);
 
 describe('Cancel notification', () => {
   it('Should be able to cancel a notification', async () => {
-    const notificationsRepository = new InMemoryNotificationsRepository();
-    const sendNotification = new SendNotification(notificationsRepository);
-    const cancelNotification = new CancelNotification(notificationsRepository);
-
     const { notification } = await sendNotification.execute({
       recipientId: 'slcknalskcna',
       content: 'Conteúdo da notificação',
@@ -20,5 +21,12 @@ describe('Cancel notification', () => {
       (notif) => notif.id === notification.id,
     );
     expect(canceledNotificatio?.canceledAt).toEqual(expect.any(Date));
+  });
+  it('Should be not able to cancel a non existing notification', async () => {
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'not-a-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
   });
 });
